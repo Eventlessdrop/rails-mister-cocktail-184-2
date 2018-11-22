d92201592e67fca2f69f703a7d1dea19caff5057
Jason Everaert <eventlessdrop8@gmail.com>
2018-11-22 19:53:49 +0900

    Now to work on the front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+ingredient['drinks'].each do |i|
+  Ingredient.create!( name: i['strIngredient1'])
+end
+
+url2 = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+cocktail_serialized = open(url2).read
+cocktail = JSON.parse(cocktail_serialized)
+
+cocktail['drinks'].each do |i|
+  Cocktail.create!( name: i['strDrink'])
+end
+
+puts "Finished"
